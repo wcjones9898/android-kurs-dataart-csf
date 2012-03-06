3c4234727c8beead20d6bb08a35c1a07e31f4657
belyj.zurab@gmail.com <belyj.zurab@gmail.com@9d566b68-6df9-dad4-a653-304a7c66b862>
2012-03-06 14:56:32 +0000

    остальная часть лабы

--- trunk/Lab3_v4_stub/trunk/src/edu/calpoly/android/lab3/JokeView.java
+++ trunk/Lab3_v4_stub/trunk/src/edu/calpoly/android/lab3/JokeView.java
@@ -8,30 +8,27 @@ import android.widget.*;
 
 public class JokeView extends LinearLayout implements View.OnClickListener, RadioGroup.OnCheckedChangeListener, Checkable {
 
-	private Button m_vwExpandButton;
-	private RadioButton m_vwLikeButton;
-	private RadioButton m_vwDislikeButton;
-	private RadioGroup m_vwLikeGroup;
-	private TextView m_vwJokeText;
-	private Joke m_joke;
+    private Button m_vwExpandButton;
+    private RadioButton m_vwLikeButton;
+    private RadioButton m_vwDislikeButton;
+    private RadioGroup m_vwLikeGroup;
+    private TextView m_vwJokeText;
+    private Joke m_joke;
     private boolean checked;
 
-	public static final String EXPAND = " + ";
-	public static final String COLLAPSE = " - ";
+    public static final String EXPAND = " + ";
+    public static final String COLLAPSE = " - ";
 
-	/**
-	 * Basic Constructor that takes only takes in an application Context.
-	 *
-	 * @param context
-	 *            The application Context in which this view is being added.
-	 *
-	 * @param joke
-	 * 			  The Joke this view is responsible for displaying.
-	 */
-	public JokeView(Context context, Joke joke) {
+    /**
+     * Basic Constructor that takes only takes in an application Context.
+     *
+     * @param context The application Context in which this view is being added.
+     * @param joke    The Joke this view is responsible for displaying.
+     */
+    public JokeView(Context context, Joke joke) {
         super(context);
         checked = false;
-        LayoutInflater inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         inflater.inflate(R.layout.joke_view, this, true);
 
         m_vwLikeButton = (RadioButton) findViewById(R.id.likeButton);
@@ -43,17 +40,16 @@ public class JokeView extends LinearLayout implements View.OnClickListener, Radi
         collapseJokeView();
         m_vwExpandButton.setOnClickListener(this);
         m_vwLikeGroup.setOnCheckedChangeListener(this);
-	}
+    }
 
-	/**
-	 * Mutator method for changing the Joke object this View displays. This View
-	 * will be updated to display the correct contents of the new Joke.
-	 *
-	 * @param joke
-	 *            The Joke object which this View will display.
-	 */
-	public void setJoke(Joke joke) {
-		m_joke = joke;
+    /**
+     * Mutator method for changing the Joke object this View displays. This View
+     * will be updated to display the correct contents of the new Joke.
+     *
+     * @param joke The Joke object which this View will display.
+     */
+    public void setJoke(Joke joke) {
+        m_joke = joke;
         m_vwJokeText.setText(joke != null ? joke.getJoke() : "");
         m_vwLikeGroup.clearCheck();
         int rating = m_joke.getRating();
@@ -61,34 +57,34 @@ public class JokeView extends LinearLayout implements View.OnClickListener, Radi
             m_vwLikeButton.setChecked(true);
         else if (rating == Joke.DISLIKE)
             m_vwDislikeButton.setChecked(false);
-	}
+    }
 
-	/**
-	 * This method encapsulates the logic necessary to update this view so that
-	 * it displays itself in its "Expanded" form:
-	 * 	- Shows the complete text of the joke.
-	 *  - Brings the RadioGroup of rating Buttons back into view.
-	 */
-	private void expandJokeView() {
-		m_vwJokeText.setEllipsize(null);
+    /**
+     * This method encapsulates the logic necessary to update this view so that
+     * it displays itself in its "Expanded" form:
+     * - Shows the complete text of the joke.
+     * - Brings the RadioGroup of rating Buttons back into view.
+     */
+    private void expandJokeView() {
+        m_vwJokeText.setEllipsize(null);
         m_vwExpandButton.setText(COLLAPSE);
         m_vwLikeGroup.setVisibility(View.VISIBLE);
         requestLayout();
-	}
+    }
 
-	/**
-	 * This method encapsulates the logic necessary to update this view so that
-	 * it displays itself in its "Collapsed" form:
-	 * 	- Shows only the first line of text of the joke.
-	 *  - If the joke is longer than one line, it appends an ellipsis to the end.
-	 *  - Removes the RadioGroup of rating Buttons from view.
-	 */
-	private void collapseJokeView() {
+    /**
+     * This method encapsulates the logic necessary to update this view so that
+     * it displays itself in its "Collapsed" form:
+     * - Shows only the first line of text of the joke.
+     * - If the joke is longer than one line, it appends an ellipsis to the end.
+     * - Removes the RadioGroup of rating Buttons from view.
+     */
+    private void collapseJokeView() {
         m_vwJokeText.setEllipsize(TextUtils.TruncateAt.END);
         m_vwExpandButton.setText(EXPAND);
         m_vwLikeGroup.setVisibility(View.GONE);
         requestLayout();
-	}
+    }
 
     @Override
     public void onClick(View view) {
